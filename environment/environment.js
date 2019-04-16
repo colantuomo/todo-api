1c196f99b56a6ec6da48cb40f9365bbc523f134e
colantuomo <pcolantuomo@gmail.com>
2019-04-15 21:18:19 -0300

    removingkeys

--- environment/environment.js
+++ environment/environment.js
@@ -1,6 +1,4 @@
 const config = {
-    OCR_URL: 'https://vision.googleapis.com/v1/images:annotate',
-    OCR_KEY: 'AIzaSyCMo2zmVFJVxXRJkicu9vHmOviuTOFtqgw',
     MONGODB_URI: 'mongodb+srv://pcolantuomo:020794%40pc@study-cluster-8b9lf.mongodb.net/study-db?retryWrites=true'
 }
 
